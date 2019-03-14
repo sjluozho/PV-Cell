7a53b0551442c28822315c0e0d15470b4119b69b
Sijin Luozhong <sjluozhong@gmail.com>
2019-03-13 17:12:14 -0700

    added rdkit module

--- PV-Cell/Data Cleaning/SeekFeatures.py
+++ PV-Cell/Data Cleaning/SeekFeatures.py
@@ -5,18 +5,21 @@ from mordred import Calculator, descriptors
 from multiprocessing import freeze_support
 
 
-def SeekFeatures(data):
+def ChemFeatures (data):
     """
         This function is used to extract chemical information from
 molecular structures in the form of Smiles_str.
 
     Attributes:
-        data: array or array like. Input data to be analysed
+        data: array or array like of SMILES_str. Input data to be
+analysed
 
     """
+    mols = [Chem.MolFromSmiles(mol) for mol in data]
     freeze_support()
     calc = Calculator(descriptors)
     print(list(calc.map(mols)))
     raw_data = calc.pandas(mols)
     features_df = pd.DataFrame(raw_data)
     return features_df
+
