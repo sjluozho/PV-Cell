d948e9a970f81fe981791647e93580ae85b64979
Zonglun Li <46466015+Zonglun-Li@users.noreply.github.com>
2019-03-22 10:17:24 -0700

    Add files via upload

--- pvcell/tests/test_seekfeatures.py
+++ pvcell/tests/test_seekfeatures.py
@@ -4,31 +4,33 @@ sys.path.append("..")
 
 
 def test_chemfeatures():
-    data = pd.read_csv('HCEPD_100K.csv')
-    data = data.head(5)
+    str = [{'a':'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'},
+           {'a':'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'}]
+    data = pd.DataFrame(str)
     import seekfeatures as sf
-    generated_features = sf.chem_features(data['SMILES_str'])
+    generated_features = sf.chem_features(data['a'])
     assert type(generated_features) == pd.core.frame.DataFrame
     assert len(generated_features) == len(data)
     return 0
 
 
 def test_missingvaluelist():
-    data = pd.read_csv('HCEPD_100K.csv')
-    data = data.head(5)
+    str = [{'a':'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'},
+           {'a':'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'}]
+    data = pd.DataFrame(str)
     import seekfeatures as sf
-    generated_features = sf.chem_features(data['SMILES_str'])
+    generated_features = sf.chem_features(data['a'])
     generated_list = sf.missing_value_list(generated_features)
     assert isinstance(generated_list, list)
-    assert 'mordred.error.Missing' in str(generated_list)
     return 0
 
 
 def test_replacemissing():
-    data = pd.read_csv('HCEPD_100K.csv')
-    data = data.head(5)
+    str = [{'a':'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'},
+           {'a':'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'}]
+    data = pd.DataFrame(str)
     import seekfeatures as sf
-    generated_features = sf.chem_features(data['SMILES_str'])
+    generated_features = sf.chem_features(data['a'])
     effective_feature = sf.replace_missing(generated_features)
     assert type(effective_feature) == pd.core.frame.DataFrame
     assert generated_features.shape == effective_feature.shape
