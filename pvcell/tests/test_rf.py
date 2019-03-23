82b92ce96f1f9dc5f9a0795b6a82ab72257b6d4e
Kailei-liu <46463618+Kailei-liu@users.noreply.github.com>
2019-03-22 21:29:18 -0700

    Update test_rf.py

--- pvcell/tests/test_rf.py
+++ pvcell/tests/test_rf.py
@@ -5,6 +5,11 @@ import sys
 sys.path.append("../")
 
 
+import seekfeatures as sf
+import processing
+import rfmodel
+
+
 def test_rf_regress():
     str = [{'SMILES_str': 'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1',
             'pce': 1},
@@ -27,12 +32,9 @@ def test_rf_regress():
            {'SMILES_str': 'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1',
             'pce': 1}]
     data = pd.DataFrame(str)
-    import seekfeatures as sf
     features_df = sf.seek_feature_with_replacement(data['SMILES_str'])
-    import processing
     X, y = processing.scale_sample(data, features_df)
     X_train, X_test, y_train, y_test = processing.kfold_sampling(X, y, n=5)
-    import rfmodel
     modelRF, RF_fit = rfmodel.rf_regress(X, y)
     assert len(modelRF) == 10
     assert len(RF_fit) == 10
