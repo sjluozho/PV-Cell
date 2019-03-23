c41c0a8c6f23e791c5ed82d7bf2b52b2274a540e
Kailei-liu <46463618+Kailei-liu@users.noreply.github.com>
2019-03-22 21:32:52 -0700

    Update test_seekfeatures.py

--- pvcell/tests/test_seekfeatures.py
+++ pvcell/tests/test_seekfeatures.py
@@ -3,11 +3,13 @@ import sys
 sys.path.append("../")
 
 
+import seekfeatures as sf
+
+
 def test_chemfeatures():
     str = [{'a': 'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'},
            {'a': 'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'}]
     data = pd.DataFrame(str)
-    import seekfeatures as sf
     generated_features = sf.chem_features(data['a'])
     # The generated features should be handled in dataframe.
     assert type(generated_features) == pd.core.frame.DataFrame
@@ -20,7 +22,6 @@ def test_missingvaluelist():
     str = [{'a': 'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'},
            {'a': 'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'}]
     data = pd.DataFrame(str)
-    import seekfeatures as sf
     generated_features = sf.chem_features(data['a'])
     generated_list = sf.missing_value_list(generated_features)
     assert isinstance(generated_list, list)
@@ -31,7 +32,6 @@ def test_replacemissing():
     str = [{'a': 'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'},
            {'a': 'C1C=CC=C1c1cc2[se]c3c4occc4c4nsnc4c3c2cn1'}]
     data = pd.DataFrame(str)
-    import seekfeatures as sf
     generated_features = sf.chem_features(data['a'])
     effective_feature = sf.replace_missing(generated_features)
     assert type(effective_feature) == pd.core.frame.DataFrame
